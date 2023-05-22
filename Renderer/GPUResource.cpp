91dedd86b328b1930363bb79818388a9dab26c69
Jan Werbrouck <janwerbrouck2000@gmail.com>
2023-05-22 16:13:17 +0100

    uploadresources require generic read

--- Renderer/GPUResource.cpp
+++ Renderer/GPUResource.cpp
@@ -16,11 +16,13 @@ GPUResource::GPUResource(ID3D12Device* pDevice, DeferredDeleteQueue* pDeleteQueu
 	// singleFrame resource
 	CD3DX12_HEAP_PROPERTIES properties = { CD3DX12_HEAP_PROPERTIES(params.heapType) };
 	CD3DX12_RESOURCE_DESC desc = { CD3DX12_RESOURCE_DESC::Buffer(params.size)};
+	D3D12_RESOURCE_STATES initialState = params.heapType == D3D12_HEAP_TYPE_UPLOAD ? D3D12_RESOURCE_STATE_GENERIC_READ : D3D12_RESOURCE_STATE_COMMON;
+
 	ThrowIfFailed(pDevice->CreateCommittedResource(
 		&properties,
 		D3D12_HEAP_FLAG_NONE,
 		&desc,
-		D3D12_RESOURCE_STATE_COMMON,
+		initialState,
 		nullptr,
 		IID_PPV_ARGS(&m_Resource)));
 }
